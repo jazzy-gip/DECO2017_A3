b0c00fa7ff63ba47f2112bcec79ec3eb5f7c1bd9
Jasmine Gip <jasmine.gip23@gmail.com>
2023-06-05 17:00:47 +1000

    '+ Set a running goal' button created to scroll to the 'Setting your goal' section below

--- public/script.js
+++ public/script.js
@@ -1,15 +1,17 @@
  
-  
   //getStartedButton element with its respective onclick event handler
   const getStartedButton = document.getElementById('getStarted');
   getStartedButton.addEventListener("click", function(event){
     scrollToSection(document.getElementById('introPageSection'));
   });
 
-
-
+  const setRunningGoalButton = document.getElementById('setRunningGoal');
+  setRunningGoalButton.addEventListener("click", function(event){
+    scrollToSection(document.getElementById('settingGoalPageSection'))
+  })
 
   //Parameter: element object supplied as argument, function causes page to scroll to specified section
   function scrollToSection(section) {
     section.scrollIntoView({ behavior: 'smooth' });
   }
+
