f8063c7e4d4a4aec6c4536c50fb0669a43f6a1db
Jasmine Gip <jasmine.gip23@gmail.com>
2023-06-05 23:11:58 +1000

    Changed classes to IDs and the implementation of javascript and draft html and css of 'today's activity box' to sidebar

--- public/script.js
+++ public/script.js
@@ -1,9 +1,22 @@
  
+var sidebarInformationContent = "";
+
   //getStartedButton element with its respective onclick event handler
   const getStartedButton = document.getElementById('getStarted');
   getStartedButton.addEventListener("click", function(event){
+    //Scroll to intro page section
     scrollToSection(document.getElementById('introPageSection'));
   });
+
+  //
+  const submitGoalButton = document.getElementById('submitGoalButton');
+  submitGoalButton.addEventListener("click", function(event){
+    //Submit data in input fields to generate sidebar activity box
+    const activityBox = document.getElementById('activityBox');
+    activityBox.innerHTML = sidebarInformationContent;
+    //get input from setting goal page section input fields and use to populate activity box
+
+  })
 
   const setRunningGoalButton = document.getElementById('setRunningGoal');
   setRunningGoalButton.addEventListener("click", function(event){
@@ -14,4 +27,13 @@
   function scrollToSection(section) {
     section.scrollIntoView({ behavior: 'smooth' });
   }
+  //return string representation of current date and time
+  function getDateString(){
+    const today = new Date();
+    return today.toLocaleDateString("en-US", {  
+        weekday: "long", year: "numeric", month: "short",  
+        day: "numeric", hour: "2-digit", minute: "2-digit"  
+    })
+  }
+  console.log(getDateString());
 
