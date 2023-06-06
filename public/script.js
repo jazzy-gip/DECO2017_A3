90ea720e30920c30488d189d4a3b628ec6f1c90a
Jasmine Gip <jasmine.gip23@gmail.com>
2023-06-06 15:59:47 +1000

    Creating functionality of recording the user's input into live tracker once 'start button' is pressed and the function of the feedback title popping up with the tracker distance circle transitioning to a green colour once the timer stops at the user's input time, signalling for completion

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+ //boolean, checks if user has set goals and submitted them
+ var sidebarPopulated = false;
+  
   //activity box info for the sidebar on the right
   const activityBoxInformation = document.getElementById('activityBoxInformation');
   //
@@ -12,8 +15,9 @@
   deleteIcon.addEventListener("click", function(event){
     //reset input fields
 
-    //hide 
+    //hide sidebar
     activityBoxInformation.hidden = true;
+    sidebarPopulated = false;
     //show default text for sidebar
     document.getElementById('activityBoxText').hidden = false;
   });
@@ -22,10 +26,8 @@
   const submitGoalButton = document.getElementById('submitGoalButton');
   submitGoalButton.addEventListener("click", function(event){
     //Submit data in input fields to generate sidebar activity box
-    //const activityBox = document.getElementById('activityBox');
-    //activityBox.innerHTML = sidebarInformationContent;
     //get input from setting goal page section input fields and use to populate activity box
-    
+    sidebarPopulated = true;
     activityBoxInformation.hidden = false;
     document.getElementById('activityBoxText').hidden = true;
 
@@ -33,8 +35,8 @@
     document.getElementById('runningModeInfo').innerHTML = document.getElementById('runningModeDropdown').value;
 
     const durationInfo = document.getElementById('durationInfo');
-    var durationString = document.getElementById('hourInput').value + ":" + document.getElementById('minuteInput').value + ":";
-    durationString = durationString + document.getElementById('secondsInput').value;
+    var durationString = formatTime(document.getElementById('hourInput').value) + ":" + formatTime(document.getElementById('minuteInput').value) + ":";
+    durationString = durationString + formatTime(document.getElementById('secondsInput').value);
     durationInfo.innerHTML = durationString;
 
     document.getElementById('distanceInfo').innerHTML = document.getElementById('targetDistanceInput').value +"km";
@@ -51,10 +53,22 @@
   var hours = 0;
   var intervalId;
 
+
   function startTimer() {
     clearInterval(intervalId);
+    //1000 millisecond polling interval, call updateTimer function each 1000 milliseconds
     intervalId = setInterval(updateTimer, 1000);
-    document.getElementById("startGoalButton").disabled = true;
+    document.getElementById("startRunningButton").disabled = true;
+  }
+  function checkTimerCompletion(){
+    if (document.getElementById('hourInput').value == hours){
+        if (document.getElementById('minuteInput').value == minutes){
+            if (document.getElementById('secondsInput').value == seconds){
+                return true;
+            }
+        }
+    }
+    return false;
   }
   function updateTimer() {
     seconds++;
@@ -68,27 +82,54 @@
     }
 
     timerElement.textContent = formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
+    //if target duration reached, stop timer, indicate completion to user
+    if (checkTimerCompletion()){
+        //change ring colour to green to indicate completion
+        document.getElementById('targetDistanceCircle').className = "circle greenBorder";
+        //indicate 'well done!' to user on screen
+        document.getElementById('startRunningTitle').innerHTML = "Well done!";
+        document.getElementById('startRunningTitle').className = "greenFont";
+        //stop timer
+        clearInterval(intervalId);
+    }
   }
   //pads 0 in front of h/m/s if it is single digit, returns padded input if time is single digit or original input
   function formatTime(time) {
+    if (time == 0) return "00";
     if (time < 10) return "0" + time;
     else return time;
   }
 
-  //Scrolling buttons
+  //on click event handlers
   const finishButtonEnd = document.getElementById('finishButton');
   finishButtonEnd.addEventListener("click", function(event){
-    scrollToSection(document.getElementById('introPageSection'))
+    scrollToSection(document.getElementById('introPageSection'));
   })
 
-  const startRunningButton = document.getElementById('startRunning');
+  const startRunningButton = document.getElementById('startRunningButton');
   startRunningButton.addEventListener("click", function(event){
-    scrollToSection(document.getElementById('startRunningPageSection'))
+    //only run code if sidebar is populated from user input
+    if (sidebarPopulated){
+        //scrolls user to start running page section
+        scrollToSection(document.getElementById('startRunningPageSection'));
+        //start duration timer
+        startTimer();
+
+        //populate start running page
+
+        var durationString = formatTime(document.getElementById('hourInput').value) + ":" + formatTime(document.getElementById('minuteInput').value) + ":";
+        durationString = durationString + formatTime(document.getElementById('secondsInput').value);
+
+        document.getElementById('targetDistance').innerHTML = document.getElementById('targetDistanceInput').value + " km";
+        document.getElementById('targetSpeed').innerHTML = document.getElementById('speedDropdown').value;
+        document.getElementById('targetDuration').innerHTML = durationString;
+    }
+    
   })
   
   const setRunningGoalButton = document.getElementById('setRunningGoal');
   setRunningGoalButton.addEventListener("click", function(event){
-    scrollToSection(document.getElementById('settingGoalPageSection'))
+    scrollToSection(document.getElementById('settingGoalPageSection'));
   })
 
   //Parameter: element object supplied as argument, function causes page to scroll to specified section
@@ -103,5 +144,4 @@
         day: "numeric", hour: "2-digit", minute: "2-digit"  
     })
   }
-  console.log(getDateString());
 
